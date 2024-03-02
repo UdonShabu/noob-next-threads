9dd715956a06303a9b4b1c42ef2c607d433f36b0
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-03-02 16:06:19 +0700

    2-SO ClerkInstanceContext

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,11 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    // TODO: Fix this error ClerkInstanceContext not found
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.className} bg-dark-1`}>{children}</body>
+    </html>
   );
 }
